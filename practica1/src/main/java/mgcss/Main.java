cdcd1391e1b648cb103da6fad43355ab67badf02
Santy <santiago.martinez@alu.uhu.es>
2016-03-10 18:40:32 +0100

    commit de 100

--- practica1/src/main/java/mgcss/Main.java
+++ practica1/src/main/java/mgcss/Main.java
@@ -10,7 +10,9 @@ package mgcss;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        System.out.println("Desesperante madre mia");
+        
+        for (int i = 1; i < 101; i++) {
             System.out.println(i);
         }
     }
