1cb8fcc298cf557425c56b7a7b379da29b317e5c
usuario <usuario>
2016-03-10 17:49:42 +0100

    Primer commit

---
+++ practica1/src/main/java/mgcss/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Mostrar por pantalla los numeros del 0 al 10
+ */
+package mgcss;
+
+/**
+ *
+ * @author Santy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
